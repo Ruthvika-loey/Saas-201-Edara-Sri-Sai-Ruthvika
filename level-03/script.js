7a352e7b30f8e83692fd5de125c87c54dbc67160
Ruthvika <ruthvikaedara@gmail.com>
2019-09-17 18:27:04 +0530

    my level 03 code

--- level-03/script.js
+++ level-03/script.js
@@ -1,109 +1,134 @@
 
 (function() {
   var initialize = function() {
-   document.getElementById("name").focus;
-   document.getElementById("department2").value=document.getElementById("department2").options[1].value;
-   document.getElementById("department2").options[0].disabled=true;
-   //console.log(document.getElementById("department2").value);
-   document.getElementById("department1").addEventListener("change",disableDuplicateSecondaryDepartment);
-   document.getElementById("submit").addEventListener("click",onsubmit);
+    
+    var button = document.querySelector(".button");
+    console.log(button);
+    button.addEventListener("click", onSubmit);
+    var department1 = document.querySelector("#department1");
+    console.log(department1);
+    department1.addEventListener('change', disableDuplicateSecondaryDepartment)
+
+  };
+  
+  var details =function()
+  {	var arr=["ECE","CIVIL","MECH"];
+    var username =document.querySelector("#username")
+    var phonenumber=document.querySelector("#phonenumber");
+    var Email=document.querySelector("#Email");
+    var department1 = document.querySelector("#department1");
+    var department2 = document.querySelector("#department2");
+		department2.value="CSE";
+	if(department1.value==="CSE")
+		department2.value=arr[Math.floor(Math.random()*2)];
+    return [username, phonenumber, Email, department1, department2];
+  };
+  
+   //Disabling option in SecondaryDepartment..
+  var disableDuplicateSecondaryDepartment = function() {
+  
+    var department1 = document.querySelector("#department1");
+    var department2 = document.querySelector("#department2");
+    var deptSelected=department1.options[department1.selectedIndex].value;
+   
+    for(var i=0;i<4;i++)
+    {
+      if(department2.options[i].disabled = true)
+      {
+        department2.options[i].disabled=false;
+      }
+    }
+    department2.options[department1.selectedIndex].disabled=true;
+    department2.selectedIndex=department1.selectedIndex+1;
+    if(department1.selectedIndex === 3)
+    {department2.selectedIndex=2;}
+    console.log(department2.selectedIndex);
+    
+
+  }
+  //Constructing Data Entered..............
+  var constructData= function() {
+    var data = {};
+    var Details=details();
+   
+    data.name=Details[0].value;
+    
+    data.phno=Details[1].value;
+    data.emailaddress=Details[2].value;
+    data.department1=Details[3].value;
+    data.department2=Details[4].value;
+
+    console.log(Details[4].value);
+
+    return data;
   };
 
-  var disableDuplicateSecondaryDepartment = function() {
-    var ind=document.getElementById("department1").selectedIndex;
-    document.getElementById("department2").options[ind].disabled=true;
-    for(i=0;i<4;i++)
-    {
-     if(i!=ind)
-      { document.getElementById("department2").options[i].disabled=false;}
-    }
-      return;
-  }
 
-  var constructData = function() {
-    var n=document.getElementById("name").value;
-    var p=document.getElementById("phno").value;
-    var em=document.getElementById("emailaddress").value;
-    var d1=document.getElementById("department1").value;
-    var d2=document.getElementById("department2").value;
-   var data={ "name":n,"phno":p,"emailaddress":em,"department1":d1,"department2":d2,}
-    return data;
-  }
+  //validate Name
+  var validateName = function(name)
+  {
+    if(name.length > 100)
+      {return false;}
+      return true;
+  };
 
+
+  //Validate Phonenumber
+   var validatePhone = function(phone)
+  {  if(phone.length > 10)
+        return false;
+      else
+      return true;
+  };
+
+ 
+  //Validate Email
+  var validateEmail = function(email)
+  {//Validating using Regularexpressions.....
+     var regx=/^([a-zA-Z0-9\.]+)(@college.edu)$/
+     console.log(regx.test(email));
+     return regx.test(email);
+  };
+
+ //validate Results
   var validateResults = function(data) {
-
-   var isValid = true;
-  var data2=Object.values(data);
-    isValid=(validatename(data2[0])&&validatephno(data2[1])&&validateemail(data2[2])&&validatedept(data2[3],data2[4]));
+    var isValid = validateName(data.name) & validatePhone(data.phno) & validateEmail(data.emailaddress);
+    console.log(isValid);
     return isValid;
   };
-  function validatedept(dpt1,dpt2)
-  {  if(dpt1==dpt2)   
-     { return false;}
-     else
-     return true;
-  }
-  function validatephno(ph)
-  {
-    var regx=/^[0-9]\d{0,9}$/;
-    var phn =document.getElementById("phno").value;
-     if(!regx.test(phn)){
-      return false;
-      }
-    }
-   function validatename(n)
-   { 
-   var regex = /^[a-zA-Z ]{0,100}$/;
-    if(regex.test(name1)){
-    return true;
-    }
-    else{
-    return false;
-    }
-    }
-    function validateemail(em)
-    {
-      var reg=/^[a-zA-Z0-9.@]+@college.edu$/;
-      if(!reg.test(mil))
-      {
-  isValid=false;
-      }
-    }
-    
-      
-   
-    var onSubmit = function(event) {
-      // 5. Figure out how to avoid the redirection on form submit
   
-      var data = constructData();
   
-      if (validateResults(data)) {
-        printResults(data);
-      } else {
-        var resultsDiv = document.getElementById("results");
-        resultsDiv.innerHTML = '';
-        resultsDiv.classList.add("hide");
-      }
-    };
   
-    var printResults = function(data) {
-      var constructElement = function([key, value]) {
-        return `<p class='result-item'>${key}: ${value}</p>`;
-      };
-  
-      var resultHtml = (Object.entries(data) || []).reduce(function(innerHtml, keyValuePair) {
-        debugger
-        return innerHtml + constructElement(keyValuePair);
-      }, '');
+  var onSubmit = function(e) {
+    e.preventDefault();
+    var data = constructData();
+    if (validateResults(data)) {
+      printResults(data);
+    } else {
       var resultsDiv = document.getElementById("results");
-      resultsDiv.innerHTML = resultHtml;
-      resultsDiv.classList.remove("hide");
+      resultsDiv.innerHTML = '';
+      resultsDiv.classList.add("hide");
+    }
+  };
+
+
+//Print Results
+  var printResults = function(data) {
+	console.log(data);
+    var constructElement = function([key,value]) {
+	
+      return `<p class='result-item'>${key}: ${value}</p>`;
     };
-  
-    /*
-      Initialize the javascript functions only after the html DOM content has loaded.
-      This is to ensure that the elements are present in the DOM before binding any event listeners to them.
-    */
-    document.addEventListener('DOMContentLoaded', initialize);
-  })();
-  
+
+    var resultHtml = (Object.entries(data) || []).reduce(function(innerHtml, keyValuePair) {
+      debugger
+      return innerHtml + constructElement(keyValuePair);
+    }, '');
+    var resultsDiv = document.getElementById("results");
+    resultsDiv.innerHTML = resultHtml;
+    resultsDiv.classList.remove("hide");
+  };
+
+  document.addEventListener('DOMContentLoaded', initialize);
+  var str='sdcdsccbdsjhcbsdhjcbsdhjcbsjchb';
+})();
